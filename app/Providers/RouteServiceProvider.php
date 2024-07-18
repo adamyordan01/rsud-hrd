cd83b1f0e3b7f3669f2c23abe6b26bc10f30893b
adamyordan01 <adamyordan0102@gmail.com>
2024-07-19 06:32:19 +0700

    change const HOME to admin/dashboard instead /home

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    // arahkan ke admin.dashboard.index
+    public const HOME = '/admin/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, etc.
