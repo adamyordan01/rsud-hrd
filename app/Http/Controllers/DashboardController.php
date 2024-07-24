194dc630dcf803b931e9e179303a7752d8b5e6d2
adamyordan01 <adamyordan0102@gmail.com>
2024-07-24 09:39:10 +0700

    add statistic on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,102 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $pegawaiPns = DB::table('hrd_karyawan')
+            ->selectRaw('count(kd_status_kerja) as jumlah')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 1 then 1 else 0 end) as laki_laki')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 0 then 1 else 0 end) as perempuan')
+            ->where('kd_status_kerja', 1)
+            ->where('status_peg', 1)
+            ->first();
+
+        $pegawaiHonor = DB::table('hrd_karyawan')
+            ->selectRaw('count(kd_status_kerja) as jumlah')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 1 then 1 else 0 end) as laki_laki')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 0 then 1 else 0 end) as perempuan')
+            ->where('kd_status_kerja', 2)
+            ->where('status_peg', 1)
+            ->first();
+
+        $pppk = DB::table('hrd_karyawan')
+            ->selectRaw('count(kd_status_kerja) as jumlah')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 1 then 1 else 0 end) as laki_laki')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 0 then 1 else 0 end) as perempuan')
+            ->where('kd_status_kerja', 7)
+            ->where('status_peg', 1)
+            ->first();
+
+        $pegawaiKontrakBlud = DB::table('hrd_karyawan')
+            ->selectRaw('count(kd_status_kerja) as jumlah')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 1 then 1 else 0 end) as laki_laki')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 0 then 1 else 0 end) as perempuan')
+            ->where('kd_status_kerja', 3)
+            ->where('status_peg', 1)
+            ->where('kd_jenis_peg', 2)
+            ->first();
+
+        $pegawaiKontrakPemko = DB::table('hrd_karyawan')
+            ->selectRaw('count(kd_status_kerja) as jumlah')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 1 then 1 else 0 end) as laki_laki')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 0 then 1 else 0 end) as perempuan')
+            ->where('kd_status_kerja', 3)
+            ->where('status_peg', 1)
+            ->where('kd_jenis_peg', 1)
+            ->first();
+
+        $pegawaiPartTime = DB::table('hrd_karyawan')
+            ->selectRaw('count(kd_status_kerja) as jumlah')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 1 then 1 else 0 end) as laki_laki')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 0 then 1 else 0 end) as perempuan')
+            ->where('kd_status_kerja', 4)
+            ->where('status_peg', 1)
+            ->first();
+
+        $pegawaiLuar = DB::table('hrd_karyawan_luar')
+            ->selectRaw('count(kd_status_kerja) as jumlah')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 1 then 1 else 0 end) as laki_laki')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 0 then 1 else 0 end) as perempuan')
+            ->where('kd_status_kerja', 5)
+            ->where('status_peg', 1)
+            ->first();
+
+        $pegawaiLuar = collect($pegawaiLuar);
+
+        $pegawaiThl = DB::table('hrd_karyawan')
+            ->selectRaw('count(kd_status_kerja) as jumlah')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 1 then 1 else 0 end) as laki_laki')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 0 then 1 else 0 end) as perempuan')
+            ->where('kd_status_kerja', 6)
+            ->where('status_peg', 1)
+            ->first();
+
+        $totalPegawai = DB::table('hrd_karyawan')
+            ->selectRaw('count(kd_status_kerja) as jumlah')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 1 then 1 else 0 end) as laki_laki')
+            ->selectRaw('sum(case when kd_jenis_kelamin = 0 then 1 else 0 end) as perempuan')
+            ->where('status_peg', 1)
+            ->first();
+
+        $pegawai = [
+            'pns' => $pegawaiPns,
+            'pppk' => $pppk,
+            'honor' => $pegawaiHonor,
+            'kontrak_blud' => $pegawaiKontrakBlud,
+            'kontrak_pemko' => $pegawaiKontrakPemko,
+            'part_time' => $pegawaiPartTime,
+            'luar' => $pegawaiLuar,
+            'thl' => $pegawaiThl,
+            'total' => $totalPegawai,
+        ];
+
+        // dd($pegawai);
+        // dd($pegawai['luar']['jumlah']);
+        
+        return view('dashboard.index', compact('pegawai'));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -55,9 +55,284 @@
 
 @section('content')
     <div id="kt_app_content" class="app-content  flex-column-fluid ">
-        <div id="kt_app_content_container" class="app-container  container-fluid ">
-            <div class="text-primary fs-4 fw-bold">
+        <div id="kt_app_content_container" class="app-container container-fluid ">
+            <div class="text-gray-600 fs-4 fw-bold mb-10">
                 Selamat Datang, {{ $nama }}
+            </div>
+
+            <div class="text-primary fs-3 fw-bold">
+                Pegawai Aktif
+            </div>
+            <div class="separator separator-dashed my-4"></div>
+            {{-- <div class="row gy-5 g-xl-5">
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="card h-lg-100">
+                        <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                            <div class="m-0">
+                                <i class="ki-duotone ki-user-square fs-2hx text-gray-600"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                            </div>
+                            <div class="d-flex flex-column my-2">
+                                <span class="fw-semibold fs-2x text-gray-700 fw-bold lh-1 ls-n2 mb-2">554 Org</span>
+                                <div class="m-0">
+                                    <span class="fw-semibold fs-6 text-gray-500">Pegawai PNS</span>
+                                </div>
+                            </div>
+                            <!-- buat dua sisi untuk menampilkan jumlah laki laki dan perempuan, 153 Laki-laki    401 Perempuan -->
+                            <div class="d-flex justify-content-between">
+                                <span class="badge badge-light-success fs-base me-2">
+                                    153 Laki-laki
+                                </span>
+                                <span class="badge badge-light-danger fs-base">
+                                    401 Perempuan
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> --}}
+            <div class="row gy-5 g-xl-5">
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="card h-lg-100">
+                        <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                            <div class="d-flex justify-content-between w-100">
+                                <div class="d-flex flex-column my-2">
+                                    <span class="fw-semibold fs-2x text-gray-700 fw-bold lh-1 ls-n2 mb-2">{{ $pegawai['pns']->jumlah }} Org</span>
+                                    <div class="m-0">
+                                        <span class="fw-semibold fs-6 text-gray-500">Pegawai PNS</span>
+                                    </div>
+                                </div>
+                                <div class="m-0">
+                                    <i class="ki-duotone ki-user-square fs-5hx text-gray-600"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                                </div>
+                            </div>
+                            <!-- buat dua sisi untuk menampilkan jumlah laki laki dan perempuan, 153 Laki-laki    401 Perempuan -->
+                            <div class="d-flex justify-content-start w-100 mt-2">
+                                <span class="badge badge-light-success fs-base me-2">
+                                    {{ $pegawai['pns']->laki_laki }} Laki-laki
+                                </span>
+                                <span class="badge badge-light-danger fs-base">
+                                    {{ $pegawai['pns']->perempuan }} Perempuan
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="card h-lg-100">
+                        <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                            <div class="d-flex justify-content-between w-100">
+                                <div class="d-flex flex-column my-2">
+                                    <span class="fw-semibold fs-2x text-gray-700 fw-bold lh-1 ls-n2 mb-2">{{ $pegawai['honor']->jumlah }} Org</span>
+                                    <div class="m-0">
+                                        <span class="fw-semibold fs-6 text-gray-500">Pegawai Honor</span>
+                                    </div>
+                                </div>
+                                <div class="m-0">
+                                    <i class="ki-duotone ki-user-square fs-5hx text-gray-600"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                                </div>
+                            </div>
+                            <!-- buat dua sisi untuk menampilkan jumlah laki laki dan perempuan, 153 Laki-laki    401 Perempuan -->
+                            <div class="d-flex justify-content-start w-100 mt-2">
+                                <span class="badge badge-light-success fs-base me-2">
+                                    {{ $pegawai['honor']->laki_laki }} Laki-laki
+                                </span>
+                                <span class="badge badge-light-danger fs-base">
+                                    {{ $pegawai['honor']->perempuan }} Perempuan
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="card h-lg-100">
+                        <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                            <div class="d-flex justify-content-between w-100">
+                                <div class="d-flex flex-column my-2">
+                                    <span class="fw-semibold fs-2x text-gray-700 fw-bold lh-1 ls-n2 mb-2">{{ $pegawai['kontrak_blud']->jumlah }} Org</span>
+                                    <div class="m-0">
+                                        <span class="fw-semibold fs-6 text-gray-500">Peg. Kontrak BLUD</span>
+                                    </div>
+                                </div>
+                                <div class="m-0">
+                                    <i class="ki-duotone ki-user-square fs-5hx text-gray-600"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                                </div>
+                            </div>
+                            <!-- buat dua sisi untuk menampilkan jumlah laki laki dan perempuan, 153 Laki-laki    401 Perempuan -->
+                            <div class="d-flex justify-content-start w-100 mt-2">
+                                <span class="badge badge-light-success fs-base me-2">
+                                    {{ $pegawai['kontrak_blud']->laki_laki }} Laki-laki
+                                </span>
+                                <span class="badge badge-light-danger fs-base">
+                                    {{ $pegawai['kontrak_blud']->perempuan }} Perempuan
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="card h-lg-100">
+                        <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                            <div class="d-flex justify-content-between w-100">
+                                <div class="d-flex flex-column my-2">
+                                    <span class="fw-semibold fs-2x text-gray-700 fw-bold lh-1 ls-n2 mb-2">{{ $pegawai['kontrak_pemko']->jumlah }} Org</span>
+                                    <div class="m-0">
+                                        <span class="fw-semibold fs-6 text-gray-500">Peg. Kontrak Pemko</span>
+                                    </div>
+                                </div>
+                                <div class="m-0">
+                                    <i class="ki-duotone ki-user-square fs-5hx text-gray-600"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                                </div>
+                            </div>
+                            <!-- buat dua sisi untuk menampilkan jumlah laki laki dan perempuan, 153 Laki-laki    401 Perempuan -->
+                            <div class="d-flex justify-content-start w-100 mt-2">
+                                <span class="badge badge-light-success fs-base me-2">
+                                    {{ $pegawai['kontrak_pemko']->laki_laki }} Laki-laki
+                                </span>
+                                <span class="badge badge-light-danger fs-base">
+                                    {{ $pegawai['kontrak_pemko']->perempuan }} Perempuan
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="card h-lg-100">
+                        <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                            <div class="d-flex justify-content-between w-100">
+                                <div class="d-flex flex-column my-2">
+                                    <span class="fw-semibold fs-2x text-gray-700 fw-bold lh-1 ls-n2 mb-2">{{ $pegawai['part_time']->jumlah }} Org</span>
+                                    <div class="m-0">
+                                        <span class="fw-semibold fs-6 text-gray-500">Pegawai Part Time</span>
+                                    </div>
+                                </div>
+                                <div class="m-0">
+                                    <i class="ki-duotone ki-user-square fs-5hx text-gray-600"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                                </div>
+                            </div>
+                            <!-- buat dua sisi untuk menampilkan jumlah laki laki dan perempuan, 153 Laki-laki    401 Perempuan -->
+                            <div class="d-flex justify-content-start w-100 mt-2">
+                                <span class="badge badge-light-success fs-base me-2">
+                                    {{ $pegawai['part_time']->laki_laki }} Laki-laki
+                                </span>
+                                <span class="badge badge-light-danger fs-base">
+                                    {{ $pegawai['part_time']->perempuan }} Perempuan
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="card h-lg-100">
+                        <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                            <div class="d-flex justify-content-between w-100">
+                                <div class="d-flex flex-column my-2">
+                                    <span class="fw-semibold fs-2x text-gray-700 fw-bold lh-1 ls-n2 mb-2">{{ $pegawai['thl']->jumlah }} Org</span>
+                                    <div class="m-0">
+                                        <span class="fw-semibold fs-6 text-gray-500">Pegawai THL</span>
+                                    </div>
+                                </div>
+                                <div class="m-0">
+                                    <i class="ki-duotone ki-user-square fs-5hx text-gray-600"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                                </div>
+                            </div>
+                            <!-- buat dua sisi untuk menampilkan jumlah laki laki dan perempuan, 153 Laki-laki    401 Perempuan -->
+                            <div class="d-flex justify-content-start w-100 mt-2">
+                                <span class="badge badge-light-success fs-base me-2">
+                                    {{ $pegawai['thl']->laki_laki }} Laki-laki
+                                </span>
+                                <span class="badge badge-light-danger fs-base">
+                                    {{ $pegawai['thl']->perempuan }} Perempuan
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="card h-lg-100">
+                        <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                            <div class="d-flex justify-content-between w-100">
+                                <div class="d-flex flex-column my-2">
+                                    <span class="fw-semibold fs-2x text-gray-700 fw-bold lh-1 ls-n2 mb-2">{{ $pegawai['pppk']->jumlah }} Org</span>
+                                    <div class="m-0">
+                                        <span class="fw-semibold fs-6 text-gray-500">PPPK</span>
+                                    </div>
+                                </div>
+                                <div class="m-0">
+                                    <i class="ki-duotone ki-user-square fs-5hx text-gray-600"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                                </div>
+                            </div>
+                            <!-- buat dua sisi untuk menampilkan jumlah laki laki dan perempuan, 153 Laki-laki    401 Perempuan -->
+                            <div class="d-flex justify-content-start w-100 mt-2">
+                                <span class="badge badge-light-success fs-base me-2">
+                                    {{ $pegawai['pppk']->laki_laki }} Laki-laki
+                                </span>
+                                <span class="badge badge-light-danger fs-base">
+                                    {{ $pegawai['pppk']->perempuan }} Perempuan
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="card h-lg-100">
+                        <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                            <div class="d-flex justify-content-between w-100">
+                                <div class="d-flex flex-column my-2">
+                                    <span class="fw-semibold fs-2x text-gray-700 fw-bold lh-1 ls-n2 mb-2">{{ $pegawai['luar']['jumlah'] }} Org</span>
+                                    <div class="m-0">
+                                        <span class="fw-semibold fs-6 text-gray-500">Total Peg. Luar RS</span>
+                                    </div>
+                                </div>
+                                <div class="m-0">
+                                    <i class="ki-duotone ki-user-square fs-5hx text-gray-600"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                                </div>
+                            </div>
+                            <!-- buat dua sisi untuk menampilkan jumlah laki laki dan perempuan, 153 Laki-laki    401 Perempuan -->
+                            <div class="d-flex justify-content-start w-100 mt-2">
+                                <span class="badge badge-light-success fs-base me-2">
+                                    {{ $pegawai['luar']['laki_laki'] }} Laki-laki
+                                </span>
+                                <span class="badge badge-light-danger fs-base">
+                                    {{ $pegawai['luar']['perempuan'] }} Perempuan
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="card h-lg-100">
+                        <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                            <div class="d-flex justify-content-between w-100">
+                                <div class="d-flex flex-column my-2">
+                                    <span class="fw-semibold fs-2x text-gray-700 fw-bold lh-1 ls-n2 mb-2">{{ $pegawai['total']->jumlah }} Org</span>
+                                    <div class="m-0">
+                                        <span class="fw-semibold fs-6 text-gray-500">Total Peg. RS</span>
+                                    </div>
+                                </div>
+                                <div class="m-0">
+                                    <i class="ki-duotone ki-user-square fs-5hx text-gray-600"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                                </div>
+                            </div>
+                            <!-- buat dua sisi untuk menampilkan jumlah laki laki dan perempuan, 153 Laki-laki    401 Perempuan -->
+                            <div class="d-flex justify-content-start w-100 mt-2">
+                                <span class="badge badge-light-success fs-base me-2">
+                                    {{ $pegawai['total']->laki_laki }} Laki-laki
+                                </span>
+                                <span class="badge badge-light-danger fs-base">
+                                    {{ $pegawai['total']->perempuan }} Perempuan
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
